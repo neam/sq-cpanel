8a78291a0896190e81f959be9b06669a214fd141
Fredrik Wollsén <fredrik@neam.se>
2012-07-26 19:41:32 +0200

    Enabled clean urls

--- yiiapp/protected/config/main.php
+++ yiiapp/protected/config/main.php
@@ -82,21 +82,18 @@ $config = array(
 			'loginUrl' => array('//user/user/login'),
 		),
 		// uncomment the following to enable URLs in path-format
-		/*
-		  'urlManager'=>array(
-		  'urlFormat'=>'path',
-		  'rules'=>array(
-		  '<controller:\w+>/<id:\d+>'=>'<controller>/view',
-		  '<controller:\w+>/<action:\w+>/<id:\d+>'=>'<controller>/<action>',
-		  '<controller:\w+>/<action:\w+>'=>'<controller>/<action>',
-		  ),
-		  ),
-		 */
-		/*
-		  'db' => array(
-		  'connectionString' => 'sqlite:' . dirname(__FILE__) . '/../data/testdrive.db',
-		  ),
-		 */
+		'urlManager' => array(
+			'urlFormat' => 'path',
+			'rules' => array(
+				'/' => 'site/index'
+			/*
+			  '<controller:\w+>/<id:\d+>'=>'<controller>/view',
+			  '<controller:\w+>/<action:\w+>/<id:\d+>'=>'<controller>/<action>',
+			  '<controller:\w+>/<action:\w+>'=>'<controller>/<action>',
+			 */
+			),
+			'showScriptName' => false
+		),
 		// uncomment the following to use a MySQL database
 		'db' => array(
 			'connectionString' => 'mysql:host=localhost;dbname=sq_cpanel',
