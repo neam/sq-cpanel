040c121774e7f4624e683e72ceb4ef71e52cf26c
Fredrik Wollsén <fredrik@neam.se>
2012-07-26 19:47:00 +0200

    Error page using error layout

--- yiiapp/protected/controllers/SiteController.php
+++ yiiapp/protected/controllers/SiteController.php
@@ -38,6 +38,8 @@ class SiteController extends Controller
 	 */
 	public function actionError()
 	{
+		$this->layout = "error";
+
 		if ($error = Yii::app()->errorHandler->error)
 		{
 			if (Yii::app()->request->isAjaxRequest)
