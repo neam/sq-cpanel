d653bb379ce973ff855dc314e1866249a5f400df
Fredrik Wollsén <fredrik@neam.se>
2012-09-14 18:25:38 -0700

    Prepared app for console commands

--- yiiapp/protected/config/console.php
+++ yiiapp/protected/config/console.php
@@ -1,24 +1,39 @@
 <?php
 
+$envbootstrap = dirname(__FILE__) . DIRECTORY_SEPARATOR . 'envbootstrap.php';
+
+if (!is_readable($envbootstrap))
+{
+	echo "Main envbootstrap file not available.";
+	die(2);
+}
+require_once($envbootstrap);
+
+//  Merge-approach from http://www.yiiframework.com/forum/index.php?/topic/23899-merging-mainphp-and-commandphp-config-files/
+$mainConfigArray = require dirname(__FILE__) . DIRECTORY_SEPARATOR . 'main.php';
+
 // This is the configuration for yiic console application.
 // Any writable CConsoleApplication properties can be configured here.
 return array(
-	'basePath'=>dirname(__FILE__).DIRECTORY_SEPARATOR.'..',
-	'name'=>'My Console Application',
+	'basePath' => dirname(__FILE__) . DIRECTORY_SEPARATOR . '..',
+	'name' => 'Selfminer Cpanel Console Application',
+	'import' => array_merge($mainConfigArray['import'], array(
+		'application.commands.components.*',
+	    )
+	),
+	// preloading 'log' component
+	'preload' => array('log'),
 	// application components
-	'components'=>array(
-		'db'=>array(
-			'connectionString' => 'sqlite:'.dirname(__FILE__).'/../data/testdrive.db',
+	'components' => array(
+		'db' => $mainConfigArray['components']['db'],
+		'log' => array(
+			'class' => 'CLogRouter',
+			'routes' => array(
+				array(
+					'class' => 'CFileLogRoute',
+					'levels' => 'error, warning',
+				),
+			),
 		),
-		// uncomment the following to use a MySQL database
-		/*
-		'db'=>array(
-			'connectionString' => 'mysql:host=localhost;dbname=testdrive',
-			'emulatePrepare' => true,
-			'username' => 'root',
-			'password' => '',
-			'charset' => 'utf8',
-		),
-		*/
 	),
 );
