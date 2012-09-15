f17d2ca8b6c82117c0ff0f7f0e4c8893d4c36392
Fredrik Wollsén <fredrik@neam.se>
2012-09-14 19:34:52 -0700

    Auto-formatting prior to editing

--- yiiapp/protected/views/virtualminHost/view.php
+++ yiiapp/protected/views/virtualminHost/view.php
@@ -2,41 +2,46 @@
 $this->breadcrumbs['Virtualmin Hosts'] = array('index');
 $this->breadcrumbs[] = $model->id;
 
-if(!$this->menu) 
-	$this->menu=array(
-			array('label'=>Yii::t('app', 'Update') , 'url'=>array('update', 'id'=>$model->id)),
-			array('label'=>Yii::t('app', 'Delete') , 'url'=>'#', 'linkOptions'=>array('submit'=>array('delete','id'=>$model->id),'confirm'=>'Are you sure you want to delete this item?')),
-			array('label'=>Yii::t('app', 'Create') , 'url'=>array('create')),
-			array('label'=>Yii::t('app', 'Manage') , 'url'=>array('admin')),
-			array('label'=>Yii::t('app', 'List') , 'url'=>array('index')),
-			);
+if (!$this->menu)
+	$this->menu = array(
+		array('label' => Yii::t('app', 'Update'), 'url' => array('update', 'id' => $model->id)),
+		array('label' => Yii::t('app', 'Delete'), 'url' => '#', 'linkOptions' => array('submit' => array('delete', 'id' => $model->id), 'confirm' => 'Are you sure you want to delete this item?')),
+		array('label' => Yii::t('app', 'Create'), 'url' => array('create')),
+		array('label' => Yii::t('app', 'Manage'), 'url' => array('admin')),
+		array('label' => Yii::t('app', 'List'), 'url' => array('index')),
+	);
 ?>
 
-<h2><?php echo Yii::t('app', 'View');?> VirtualminHost <?php echo $model->id; ?></h2>
+<h2><?php echo Yii::t('app', 'View'); ?> VirtualminHost <?php echo $model->id; ?></h2>
 
-<?php $this->widget('zii.widgets.CDetailView', array(
-'data'=>$model,
-	'attributes'=>array(
-					'id',
+<?php
+$this->widget('zii.widgets.CDetailView', array(
+	'data' => $model,
+	'attributes' => array(
+		'id',
 		'host',
 		'user',
 		'pass',
 		'created',
-),
-	)); ?>
+	),
+));
+?>
 
 
-	<h2><?php echo CHtml::link(Yii::t('app','Containers'), array('/container/admin'));?></h2>
+<h2><?php echo CHtml::link(Yii::t('app', 'Containers'), array('/container/admin')); ?></h2>
 <ul>
-				<?php if (is_array($model->containers)) foreach($model->containers as $foreignobj) { 
+	<?php
+	if (is_array($model->containers))
+		foreach ($model->containers as $foreignobj)
+		{
 
-					echo '<li>';
-					echo CHtml::link($foreignobj->_label, array('/container/view','id'=>$foreignobj->id));
-							
-						echo ' '.CHtml::link(Yii::t('app','Update'), array('/container/update','id'=>$foreignobj->id), array('class'=>'edit'));
+			echo '<li>';
+			echo CHtml::link($foreignobj->_label, array('/container/view', 'id' => $foreignobj->id));
 
-				}
-			?></ul><p><?php echo CHtml::link(
-				Yii::t('app','Create'),
-				array('/container/create', 'Container' => array('virtualmin_host_id'=>$model->{$model->tableSchema->primaryKey}))
-					);  ?></p>
+			echo ' ' . CHtml::link(Yii::t('app', 'Update'), array('/container/update', 'id' => $foreignobj->id), array('class' => 'edit'));
+		}
+	?></ul><p><?php
+	echo CHtml::link(
+	    Yii::t('app', 'Create'), array('/container/create', 'Container' => array('virtualmin_host_id' => $model->{$model->tableSchema->primaryKey}))
+	);
+	?></p>
